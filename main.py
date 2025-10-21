f0506eafebcfa9e08362c6727cf2674671d80509
Sherenok12 <sherenok12@gmail.com>
2025-10-21 19:48:53 +0300

    Implement main function to display numbers and calculations

---
+++ main.py
@@ -0,0 +1,11 @@
+from math_utils import factorial, average
+
+def main():
+    numbers = [2, 4, 6]
+    print("Список чисел:", numbers)
+    print("Середнє значення:", average(numbers))
+    print("Факторіал першого числа:", factorial(numbers[0]))
+
+if __name__ == "__main__":
+    main()
+ 

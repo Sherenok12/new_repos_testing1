d7505bfdf496ce7870e93542672feda49214ca66
Sherenok12 <sherenok12@gmail.com>
2025-11-04 20:10:34 +0200

    Enhance main.py with additional math utilities
    Added new utility functions to main.py and updated the main function to use them.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from math_utils import factorial, average
+from math_utils import factorial, average, is_even, max_difference, normalize
+
 
 def main():
-    numbers_mas = [2, 4, 6]  # fixed name
+    numbers_mas = [2, 4, 6]
     print("Список чисел:", numbers_mas)
+
     print("Середнє значення:", average(numbers_mas))
     print("Факторіал першого числа:", factorial(numbers_mas[0]))
+    print("Чи перше число парне?:", is_even(numbers_mas[0]))
+    print("Різниця між max і min:", max_difference(numbers_mas))
+    print("Нормалізовані значення:", normalize(numbers_mas))
+
+    try:
+        factorial(-3)
+    except ValueError as e:
+        print("Помилка:", e)
+
 
 if __name__ == "__main__":
     main()
- 

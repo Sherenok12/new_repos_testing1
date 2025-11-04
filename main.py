585e28064db58438fc4d30a70b30d19d70e0e692
Sherenok12 <sherenok12@gmail.com>
2025-11-04 19:46:58 +0200

    fix: rename variable and improve formatting

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from math_utils import factorial, average
 
 def main():
-    numbers = [2, 4, 6]
-    print("Список чисел:", numbers)
-    print("Середнє значення:", average(numbers))
-    print("Факторіал першого числа:", factorial(numbers[0]))
+    numbers_mas = [2, 4, 6]  # fixed name
+    print("Список чисел:", numbers_mas)
+    print("Середнє значення:", average(numbers_mas))
+    print("Факторіал першого числа:", factorial(numbers_mas[0]))
 
 if __name__ == "__main__":
     main()
